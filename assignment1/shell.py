5e209982ee98a91bd15201d178117703a02bfcdb
Kirill Kuyan <progingisfun@gmail.com>
2020-05-30 00:07:03 +0300

    Refactoring

--- assignment1/shell.py
+++ assignment1/shell.py
@@ -1,7 +1,15 @@
 import sys
 import os
+from datetime import datetime as dt
 
 EXIT_MESSAGE = 'Goodbye!'
+LOG_FILE_NAME = 'myshell'
+LOGS_DIRECTORY = 'logs'
+OUT_DIRECTORY = LOGS_DIRECTORY + '/out'
+ERR_DIRECTORY = LOGS_DIRECTORY + '/err'
+
+current_outlog_file = ''
+current_errlog_file = ''
 
 def path_cutter(path):
     path_split = path.split('/')
@@ -29,16 +37,46 @@ def log():
     #TODO
     return
 
+def sys_info():
+    os.system('python3 -V')
 
-while True:
-    current_path = '[' + path_cutter(os.getcwd()) + ']'
-    try:
-        cmd = input('L_Shell {0}: '.format(current_path))
-        process(cmd)
-    except EOFError:
-        print("^D")
-        print(EXIT_MESSAGE)
-        break
-    except StopIteration:
-        print(EXIT_MESSAGE)
-        break
+def init():
+    def create_log_file():
+        if not (os.path.exists(LOGS_DIRECTORY)):
+            os.makedirs(LOGS_DIRECTORY)
+
+        if not (os.path.exists(OUT_DIRECTORY)):
+            os.makedirs(OUT_DIRECTORY)
+
+        if not (os.path.exists(ERR_DIRECTORY)):
+            os.makedirs(ERR_DIRECTORY)
+
+        current_date = dt.today().strftime('%d%m%Y')
+        current_log_file = OUT_DIRECTORY + '_' + current_date + '.log'
+        current_errlog_file = ERR_DIRECTORY + '_' + current_date + '.stderr'
+
+
+    # create_log_file()
+    # sys_info()
+    read()
+
+def read():
+    while True:
+        current_path = '[' + path_cutter(os.getcwd()) + ']'
+        try:
+            cmd = input('L_Shell {0}: '.format(current_path))
+            process(cmd)
+        except EOFError:
+            print("^D")
+            print(EXIT_MESSAGE)
+            break
+        except StopIteration:
+            print(EXIT_MESSAGE)
+            break
+        except FileNotFoundError as e:
+            print(e)
+
+def main():
+    init()
+    
+main()
