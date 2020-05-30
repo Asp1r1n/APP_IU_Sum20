f63bb688413db7cac8b6dea18b7c65103a6acd5b
Kirill Kuyan <progingisfun@gmail.com>
2020-05-30 14:32:03 +0300

    Task 3,4,5 & Refactoring

--- assignment1/shell.py
+++ assignment1/shell.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import platform
+import subprocess
 from datetime import datetime as dt
 
 EXIT_MESSAGE = 'Goodbye!'
@@ -33,22 +34,54 @@ def process(cmd):
             os.chdir(os.path.expanduser('~'))
 
     def command(cmd):
-        os.system(cmd)
+        executed = subprocess.Popen(cmd.split(' '), stdout=subprocess.PIPE, universal_newlines = True)
+        out = executed.stdout
+
+        def running():
+            datetime = dt.now()
+
+            lines_out = 0
+            while True:
+                try:
+                    output = out.readline()
+                    print(output, end='')
+                    lines_out += 1
+                    
+                    return_code = executed.poll()
+                    if (return_code is not None): 
+                        lines = out.readlines()
+                        for output in lines:
+                            print(output, end='')
+                            lines_out += 1
+                        break
+                except KeyboardInterrupt:
+                    print()
+                    break
+
+            log(str_log(executed, datetime.strftime('%Y-%m-%d %H:%M:%S'), lines_out), OUT_LOG_FILE)
+            
+        running()
+
 
     if (cmd == 'exit()'): exit()
     elif (cmd.startswith('cd')): cd()
     else: command(cmd)
 
+def log(str, where = sys.stdout):
+    if not (where  == sys.stdout): print(str, file = where, flush = True)
+    else: print(str)
 
+def str_log(process, timestamp, stdout_count = 0):
+    cmd = process.args[0]
+    args = process.args[1:] if len(process.args) > 1 else []
+    pid = process.pid
+    exit_code = process.returncode
 
-
-def err(exception):
-    ERR_LOG_FILE.write(str(exception) + "\n")
-    ERR_LOG_FILE.flush()
-
+    log_string = '[{0}] cmd: {1}, args: {2}, stdout: {3}, pid: {4}, exit: {5}'.format(timestamp, cmd, args, stdout_count, pid, exit_code )
+    return log_string
 
 def sys_info():
-    print('Python shell run platform: ' + platform.system() + ', Version: ' + str(platform.linux_distribution()) + ', Python: ' + platform.python_version())
+    print('Python shell run platform: ' + platform.system() + ', Version: ' + str(platform.linux_distribution()) + ', Python ' + platform.python_version())
 
 
 def init():
@@ -65,20 +98,24 @@ def init():
         current_date = dt.today().strftime('%d%m%Y')
 
         global current_outlog_file
-        current_log_file = OUT_DIRECTORY + '/'+LOG_FILE_NAME + '_' + current_date + '.log'
+        current_outlog_file = OUT_DIRECTORY + '/' +LOG_FILE_NAME + '_' + current_date + '.log'
 
         global current_errlog_file
-        current_errlog_file = ERR_DIRECTORY + '/'+LOG_FILE_NAME + '_' + current_date + '.stderr'
+        current_errlog_file = ERR_DIRECTORY + '/' +LOG_FILE_NAME + '_' + current_date + '.stderr'
         
         global ERR_LOG_FILE
         ERR_LOG_FILE = open(current_errlog_file, "a")
+
+        global OUT_LOG_FILE
+        OUT_LOG_FILE = open(current_outlog_file, "a")
 
     create_log_file()
     sys_info()
     read()
 
 def end():
-    ERR_LOG_FILE.close()
+    if not ERR_LOG_FILE.closed: ERR_LOG_FILE.close()
+    if not OUT_LOG_FILE.closed: OUT_LOG_FILE.close()
 
 def read():
     while True:
@@ -90,15 +127,21 @@ def read():
             print("^D")
             print(EXIT_MESSAGE)
             break
+        except KeyboardInterrupt:
+            print()
+            print(EXIT_MESSAGE)
+            break
         except StopIteration:
             print(EXIT_MESSAGE)
             break
         except Exception as e:
-            err(e)
+            log(e, ERR_LOG_FILE)
 
     end()
+
 
 def start():
     init()
     
+
 start()
