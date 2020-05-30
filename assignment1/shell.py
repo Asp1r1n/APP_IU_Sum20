1eca0a741029bcffda29ce6b4aa7c27494991d9f
Kirill Kuyan <progingisfun@gmail.com>
2020-05-30 10:27:40 +0300

    fix and refactor

--- assignment1/shell.py
+++ assignment1/shell.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import platform
 from datetime import datetime as dt
 
 EXIT_MESSAGE = 'Goodbye!'
@@ -12,6 +13,7 @@ ERR_DIRECTORY = LOGS_DIRECTORY + '/err'
 current_outlog_file = ''
 current_errlog_file = ''
 ERR_LOG_FILE = ''
+OUT_LOG_FILE = ''
 
 def path_cutter(path):
     path_split = path.split('/')
@@ -25,7 +27,7 @@ def process(cmd):
 
     def cd():
         directory = cmd.split(' ')
-        if len(directory) > 1:
+        if (len(directory) > 1):
             os.chdir(directory[1])
         else:
             os.chdir(os.path.expanduser('~'))
@@ -33,20 +35,20 @@ def process(cmd):
     def command(cmd):
         os.system(cmd)
 
-    if cmd == 'exit()':
-        exit()
-    elif cmd.startswith('cd'):
-        cd()
-    else:
-        command(cmd)
+    if (cmd == 'exit()'): exit()
+    elif (cmd.startswith('cd')): cd()
+    else: command(cmd)
 
 
-def log(exception):
+
+
+def err(exception):
     ERR_LOG_FILE.write(str(exception) + "\n")
+    ERR_LOG_FILE.flush()
 
 
 def sys_info():
-    os.system('python3 -V')
+    print('Python shell run platform: ' + platform.system() + ', Version: ' + str(platform.linux_distribution()) + ', Python: ' + platform.python_version())
 
 
 def init():
@@ -61,21 +63,28 @@ def init():
             os.makedirs(ERR_DIRECTORY)
 
         current_date = dt.today().strftime('%d%m%Y')
+
+        global current_outlog_file
         current_log_file = OUT_DIRECTORY + '/'+LOG_FILE_NAME + '_' + current_date + '.log'
+
         global current_errlog_file
         current_errlog_file = ERR_DIRECTORY + '/'+LOG_FILE_NAME + '_' + current_date + '.stderr'
+        
         global ERR_LOG_FILE
         ERR_LOG_FILE = open(current_errlog_file, "a")
 
     create_log_file()
-    # sys_info()
+    sys_info()
     read()
+
+def end():
+    ERR_LOG_FILE.close()
 
 def read():
     while True:
-        current_path = '[' + path_cutter(os.getcwd()) + ']'
+        current_path =  path_cutter(os.getcwd())
         try:
-            cmd = input('L_Shell {0}: '.format(current_path))
+            cmd = input('L_Shell [{0}]: '.format(current_path))
             process(cmd)
         except EOFError:
             print("^D")
@@ -85,9 +94,11 @@ def read():
             print(EXIT_MESSAGE)
             break
         except Exception as e:
-            log(e)
+            err(e)
 
-def main():
+    end()
+
+def start():
     init()
     
-main()
+start()
