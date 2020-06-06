7073ecdb4415cfed132e026a508d1da925370c3f
Kirill Kuyan <progingisfun@gmail.com>
2020-06-06 23:40:37 +0300

    Fix pyc dis

--- assignment2/task3.py
+++ assignment2/task3.py
@@ -64,9 +64,23 @@ def dis_py():
         print_dis(file_name, arg)
 
 def dis_pyc():
+    header_sizes = [
+        # (size, first version this applies to)
+        # pyc files were introduced in 0.9.2 way, way back in June 1991.
+        (8,  (0, 9, 2)),  # 2 bytes magic number, \r\n, 4 bytes UNIX timestamp
+        (12, (3, 6)),     # added 4 bytes file size
+        # bytes 4-8 are flags, meaning of 9-16 depends on what flags are set
+        # bit 0 not set: 9-12 timestamp, 13-16 file size
+        # bit 0 set: 9-16 file hash (SipHash-2-4, k0 = 4 bytes of the file, k1 = 0)
+        (16, (3, 7)),     # inserted 4 bytes bit flag field at 4-8 
+        # future version may add more bytes still, at which point we can extend
+        # this table. It is correct for Python versions up to 3.9
+    ]
+    header_size = next(s for s, v in reversed(header_sizes) if sys.version_info >= v)
+
     for arg in current_args:
         with open(arg, 'rb') as src:
-            src.seek(12)
+            src.seek(header_size)
             source = marshal.load(src)
     
         file_name = dis_file_name(arg)
