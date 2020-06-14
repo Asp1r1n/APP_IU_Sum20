12f2de9afacd68d8c6198e86cfef93dc2a99983f
Kirill Kuyan <progingisfun@gmail.com>
2020-06-14 15:34:58 +0300

    A4 Docs_normalizer

--- assignment4/lanalyzer.py
+++ assignment4/lanalyzer.py
@@ -18,6 +18,64 @@ str_chs = ['"',"'"]
 def list_as_str(l:list, jch = ''):
     return jch.join(l)
 
+def normalize_docs(tokens:list):
+    empty_str_lit = ['""',"''"]
+
+    new_tokens = []
+
+    start_indx = -1;
+    end_indx = -1;
+    is_empty_str_lit = False
+    doc_ch = ''
+
+    is_doc_token = False
+    doc_token = ''
+
+    for indx,token in enumerate(tokens):
+        if token in empty_str_lit:
+            if not is_empty_str_lit:
+                doc_ch = token[0]
+                start_indx = indx
+                is_empty_str_lit = True
+            else:
+                if token[0] == doc_ch:
+                    end_indx = indx
+                    is_empty_str_lit = False
+ 
+        if start_indx != -1 and not is_doc_token:
+            if start_indx == indx - 1:
+                if token.startswith(doc_ch):
+                    is_doc_token = True
+                    doc_token = tokens[start_indx] + token
+                else:
+                    is_empty_str_lit = False
+                    is_doc_token = False
+                    new_tokens.append(tokens[start_indx])
+                    start_indx = -1
+                    end_indx = -1
+
+        if is_doc_token:
+            if end_indx == -1:
+                if start_indx != indx - 1:
+                    doc_token += ' ' + token
+            else:
+                doc_token += token
+                is_empty_str_lit = False
+                is_doc_token = False
+                start_indx = -1
+                end_indx = -1
+                new_tokens.append(doc_token)
+                doc_token = ''
+        elif start_indx == -1 and not is_doc_token:
+            new_tokens.append(token)
+
+    if doc_token != '':
+        if tokens[0] in empty_str_lit:
+            new_tokens.append(doc_token)
+        elif tokens[len(tokens) - 2] in empty_str_lit:
+            new_tokens = [' '.join(tokens[0:len(tokens) - 2]) + doc_token]
+        
+    return new_tokens
 
 def tokens(line):
 
@@ -30,41 +88,42 @@ def tokens(line):
 
     str_lit_ch  = ''
     c_word = []
-    for indx,ch in enumerate(line):
+
+    def add_token():
+        tokens.append(list_as_str(c_word))
+        c_word.clear()
+
+
+    for ch in line:
 
         if ch == ' ' and not is_comment: is_word = False; is_delim = False; is_comment = False
         elif ch in str_chs:
             if not is_str_lit: 
                 if c_word != []:
-                    tokens.append(list_as_str(c_word))
-                    c_word.clear()
+                    add_token()
                 
                 str_lit_ch = ch
                 is_str_lit = True
             else:
                 if ch == str_lit_ch:
                     c_word.append(ch)
-                    tokens.append(list_as_str(c_word))
-                    c_word.clear()
+                    add_token()
                     is_str_lit = False
                     continue
         elif ch == cmm_ch:
             if c_word != []: 
-                tokens.append(list_as_str(c_word))
-                c_word.clear()
+                add_token()
             is_comment = True;
         elif ch not in PY_DELIMETERS_S:
             if is_delim and not is_str_lit:
                 if c_word != []:
-                    tokens.append(list_as_str(c_word))
-                    c_word.clear()
+                    add_token()
                 is_delim = False
             is_word = True
         else:
             if is_word and not is_str_lit:
                 if c_word != []:
-                    tokens.append(list_as_str(c_word))
-                    c_word.clear()
+                    add_token()
                 is_word = False
             is_delim = True
         
@@ -78,25 +137,38 @@ def tokens(line):
              c_word.append(ch)
              if is_delim:
                  if list_as_str(c_word) in DELIMETERS_S:
-                     tokens.append(ch)
-                     c_word.clear()
-
+                     add_token()
         if ch == ' ' and c_word != []:
             if not is_comment and not is_str_lit:
-                tokens.append(list_as_str(c_word))
-                c_word.clear()
+                add_token()
     
     if c_word != []:
-        tokens.append(list_as_str(c_word))
+        add_token()
         
     return tokens
+
+
+def startswith(line_tokens:list, starts:list):
+    if line_tokens != []: return line_tokens[0] in starts
+
+def calc_operators(lines:list, operators:list):
+    ops_dict = dict.fromkeys(operators, 0)
+
+    for line in lines:
+        line_tokens = tokens(line)
+        # TODO
+
+    return ops_dict
+    
 
 
 input_file = fileinput.input()
 
 for line in input_file:
     tokens_list = tokens(line.strip())
-    print(tokens_list)
+    mn_list = normalize_docs(tokens_list)
+    print(line.strip())
+    print('--->' + str(mn_list))
 
 
 
