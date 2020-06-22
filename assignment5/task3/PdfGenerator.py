dd2d0362dba2d36e30b515185ca90af0657d0e60
Agurall <shenya_59_59@mail.ru>
2020-06-22 12:54:13 +0300

    task 3 pdf gen added

--- assignment5/task3/PdfGenerator.py
+++ assignment5/task3/PdfGenerator.py
@@ -1,4 +1,7 @@
+
+import matplotlib.pyplot as plt
 from fpdf import FPDF
+import numpy as np
 
 
 class PdfPrinter:
@@ -7,8 +10,7 @@ class PdfPrinter:
     def __init__(self):
         self.pdf.set_font('Arial', 'B', 12)
 
-
-    def print_dictionaries(self, *dictionaris : dict):
+    def print_dictionaries(self, *dictionaris: dict):
         self.pdf.add_page()
 
         for dict in dictionaris:
@@ -16,7 +18,7 @@ class PdfPrinter:
             self.__print_dictionary(dict)
             self.pdf.cell(0, 10, "}", 0, 1, "L")
 
-    def __print_dictionary(self, dictionary : dict):
+    def __print_dictionary(self, dictionary: dict):
         for rec in dictionary.keys():
             txt = str(rec) + " : "
             if type(dictionary[rec]) is dict:
@@ -27,6 +29,20 @@ class PdfPrinter:
                 txt += str(dictionary[rec])
                 self.pdf.cell(0, 10, txt, 0, 1, "L")
 
+    def print_bar_chart(self, name: str, xlable: str, ylable: str, dictionary: dict):
+        objects = list(dictionary.keys())
+        y_pos = np.arange(len(objects))
+        performance = list(dictionary.values())
 
-    def save_file(self, filename : str):
+        plt.bar(y_pos, performance, align='center', alpha=0.5)
+        plt.xticks(y_pos, objects)
+        plt.ylabel(ylable)
+        plt.xlabel(xlable)
+        plt.title(name)
+
+        plt.savefig('tmp.png')
+
+        self.pdf.image('tmp.png', w= 200, type='png')
+
+    def save_file(self, filename: str):
         self.pdf.output(filename)
--- assignment5/task3/Test.py
+++ assignment5/task3/Test.py
@@ -1,5 +1,7 @@
 from PdfGenerator import PdfPrinter
 
+
 printer = PdfPrinter()
 printer.print_dictionaries({"A" : { "A1" : "B1" , "A2":"B2"}, "V" : "C1"})
+printer.print_bar_chart("test", "x","y", {"A":1,"B":2})
 printer.save_file("change_fonts.pdf")
