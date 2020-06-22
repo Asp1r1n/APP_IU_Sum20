9792bc2fe8a7199b0d75e86b9081ac9e5b4ff03e
Agurall <shenya_59_59@mail.ru>
2020-06-22 13:42:43 +0300

    task 2 pdf generator updated

--- assignment5/task2_/PdfGenerator.py
+++ assignment5/task2_/PdfGenerator.py
@@ -8,24 +8,31 @@ class PdfPrinter:
         self.pdf.set_font('Arial', 'B', 12)
 
 
-    def print_dictionaries(self, *dictionaris : dict):
+    def print_dictionaries(self, *dictionaris: dict):
         self.pdf.add_page()
 
         for dict in dictionaris:
-            self.pdf.cell(0, 10, "{", 0, 1, "L")
+            self.pdf.cell(0, 10, " ", 0, 1, "L")
             self.__print_dictionary(dict)
-            self.pdf.cell(0, 10, "}", 0, 1, "L")
+            self.pdf.cell(0, 10, " ", 0, 1, "L")
 
-    def __print_dictionary(self, dictionary : dict):
+    def __print_dictionary(self, dictionary: dict, offset = 1):
         for rec in dictionary.keys():
-            txt = str(rec) + " : "
+            txt = (" " * offset) + str(rec) + " : "
             if type(dictionary[rec]) is dict:
-                self.pdf.cell(0, 10, txt + "{", 0, 1, "L")
-                self.__print_dictionary(dictionary[rec])
-                self.pdf.cell(0, 10, "}", 0, 1, "L")
+                self.pdf.cell(0, 10, txt + " ", 0, 1, "L")
+                self.__print_dictionary(dictionary[rec], offset+1)
+                self.pdf.cell(0, 10, " ", 0, 1, "L")
             else:
-                txt += str(dictionary[rec])
-                self.pdf.cell(0, 10, txt, 0, 1, "L")
+                if len(str(dictionary[rec]).split("\n")) == 1 :
+                    txt += str(dictionary[rec])
+                    self.pdf.cell(0, 10, txt, 0, 1)
+                else:
+                    txt += str('"')
+                    self.pdf.cell(0, 10, txt, 0, 1)
+                    for line in str(dictionary[rec]).split("\n"):
+                        self.pdf.cell(0, 10, (" " * offset) + line, 0, 1)
+                    self.pdf.cell(0, 10, (" " * offset) +'"', 0, 1)
 
 
     def save_file(self, filename : str):
